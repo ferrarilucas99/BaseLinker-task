403725c72e173d3d9a196f1eee640bba7a12a3f2
Lucas Ferrari <lucashols65@gmail.com>
2024-09-25 20:46:14 -0300

    implementing the newPackage function to create a shipment

--- index.php
+++ index.php
@@ -3,7 +3,47 @@
 require_once "./spring.php";
 
 $springCourier = new SpringCourier();
+$randomRef = "Ref_" . bin2hex(random_bytes(8/2));
 
-$springCourier->newPackage([], []);
+$order = [
+    "sender" => [
+        "Name" => "Jan Kowalski",
+        "Company" => "BaseLinker",
+        "AddressLine1" => "Kopernika 10",
+        "City" => "Gdansk",
+        "State" => "",
+        "Zip" => "80208",
+        "Country" => "PL",
+        "Phone" => "666666666",
+        "Email" => "jankowalski@test.com",
+    ],
+    "recipient" => [
+        "Name" => "Maud Driant",
+        "Company" => "Spring GDS",
+        "AddressLine1" => "Strada Foisorului, Nr. 16, Bl. F11C, Sc. 1, Ap. 10",
+        "City" => "Bucuresti, Sector 3",
+        "State" => "",
+        "Zip" => "031179",
+        "Country" => "RO",
+        "Phone" => "555555555",
+        "Email" => "mauddriant@test.com",
+    ]
+];
+
+$params = [
+    "apiKey" => "f16753b55cac6c6e",
+    "labelFormat" => "PDF",
+    "service" => "PPTT",
+    "shipperReference" => $randomRef
+];
+
+$newPackage = $springCourier->newPackage($order, $params);
+
+if (isset($newPackage["error"]) && $newPackage["error"]) {
+    echo $newPackage["message"];
+} else {
+    echo "Tracking Number: " . $newPackage["Shipment"]["TrackingNumber"];
+}
+
 echo "<br>";
 $springCourier->packagePDF("");
--- spring.php
+++ spring.php
@@ -2,13 +2,114 @@
 
 class SpringCourier
 {
-    public function newPackage(array $order, array $params)
+    const API_URL = "https://mtapi.net/?testMode=1";
+
+    /**
+     * Create a new shipment
+     * 
+     * @param array $order Associative array that contains the sender and recipient data
+     * @param array $params Associative array that contains additional data
+     *  to create the new shipment (API key, service...)
+     * 
+     * @return array Associative array containing the data returned 
+     * from the API or connection error
+     */
+    public function newPackage(array $order, array $params): array
     {
-        echo __FUNCTION__;
+        $data = [
+            "Apikey" => $params["apiKey"],
+            "Command" => "OrderShipment",
+            "Shipment" => [
+                "LabelFormat" => $params["labelFormat"],
+                "ShipperReference" => $params["shipperReference"],
+                "Service" => $params["service"],
+                "ConsignorAddress" => $order["sender"],
+                "ConsigneeAddress" => $order["recipient"],
+            ]
+        ];
+
+        $response = $this->curlExec($data);
+
+        if ($response["ErrorLevel"] != 0) {
+            return [
+                "error" => true,
+                "message" => "Error creating shipment: {$response['Error']}"
+            ];
+        }
+
+        return $response;
     }
 
     public function packagePDF(string $trackingNumber)
     {
         echo __FUNCTION__;
+    }
+
+    /**
+     * Execute a connection to the API using curl
+     * 
+     * @param array $data Associative array containing the data that will be sent to the API
+     * 
+     * @return array Associative array containing the API response or connection error
+     */
+    public function curlExec(array $data): array
+    {
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL => self::API_URL,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_SSL_VERIFYPEER => $this->isHttps(),
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_HTTPHEADER => [
+                "Content-Type: text/json",
+            ],
+        ]);
+
+        $response = curl_exec($ch);
+        $error = curl_error($ch);
+
+        curl_close($ch);
+
+        if (!empty($error)) {
+            return [
+                "ErrorLevel" => 2,
+                "Error" => "Connection error: {$error}"
+            ];
+        }
+
+        return json_decode($response, true);
+    }
+
+    /**
+     * Checks if the connection is https or https
+     * 
+     * @return bool true for https connection or false for http connection
+     */
+    private function isHttps(): bool
+    {
+        if (
+            $_SERVER["SERVER_NAME"] === ("localhost" || "127.0.0.1" || "0.0.0.0")
+            || $_SERVER["SERVER_PORT"] !== "443"
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Temporary function to debug and die with <pre> element
+     */
+    public function dd(...$data)
+    {
+        echo "<pre>";
+        var_dump(...$data);
+        echo "</pre>";
+        die;
     }
 }
